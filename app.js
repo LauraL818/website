41136925db3c2c40565dcc46282fabd05f3be733
Laura Leonard <Laura.Leonard818@gmail.com>
2016-04-11 17:32:32 -0700

    added navbar

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 $(function(){
+  var $about = $("#about")
+  var $projects = $("#projects")
+  var $contact = $("#contact")
+  var $home = $("#home")
+  var $aboutPage = $(".about")
+  var $projectsPage = $(".projects")
+  var $contactPage = $(".contact")
+  var $homePage = $(".home")
 
-setTimeout(
-  function(){
-    $(".name").typed({
-          strings: ["LAURA ^500 LEONARD"],
-          typeSpeed: 50
-      })}, 1000)
+  $aboutPage.hide()
+  $projectsPage.hide()
+  $contactPage.hide()
+
+/////////////////////FUNCTION FOR TYPING NAME/////////////////////////
+  setTimeout(
+    function(){
+      $(".name").typed({
+            strings: ["LAURA ^500 LEONARD"],
+            typeSpeed: 50
+        })}, 700)
+        
+/////////////////////FUNCTIONS FOR SHOWING AND HIDING/////////////////////////
+  $about.click(function(){
+    $aboutPage.show()
+    $projectsPage.hide()
+    $contactPage.hide()
+    $homePage.hide()
+  })
+
+  $projects.click(function(){
+    $projectsPage.show()
+    $aboutPage.hide()
+    $contactPage.hide()
+    $homePage.hide()
+  })
+
+  $contact.click(function(){
+    $contactPage.show()
+    $aboutPage.hide()
+    $projectsPage.hide()
+    $homePage.hide()
+  })
+
+  $home.click(function(){
+    $homePage.show()
+    $aboutPage.hide()
+    $projectsPage.hide()
+    $contactPage.hide()
+  })
+
 })
