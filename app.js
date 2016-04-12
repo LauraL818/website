7b05e7359d6c854a66ffa646350b05568eba2028
Laura Leonard <Laura.Leonard818@gmail.com>
2016-04-11 17:01:47 -0700

    fixed typed js timing

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 $(function(){
+
+setTimeout(
+  function(){
     $(".name").typed({
-        strings: ["LAURA ^500 LEONARD"],
-        typeSpeed: 50
-    })
+          strings: ["LAURA ^500 LEONARD"],
+          typeSpeed: 50
+      })}, 500)
 })
